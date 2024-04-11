096ce067c39c892935aa9ab36faafa0dad621244
Dakouri M. Kobri <maurille.kobri@gmail.com>
2024-04-11 03:08:27 +0200

    Initialize projet

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <h1>My Money</h1>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+// NPM Packages
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// Project Imports
+import App from './App.jsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
