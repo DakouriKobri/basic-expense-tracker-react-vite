bd77e58ef6212f40457f157f8ed5cd97b877022a
Dakouri M. Kobri <maurille.kobri@gmail.com>
2024-04-14 03:39:36 +0200

    Add separator line to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 export default function Footer() {
   return (
     <footer className="footer">
+      <div className="footer__line"></div>
       This website was built for educational purpose only &#124; &copy; 2024
       Dakouri Kobri
     </footer>
